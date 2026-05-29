4988948151fc1a05f7f16c7b8cde0ed73ebb0614
milindpks98-star <milind.pks98@gmail.com>
2026-05-30 01:36:35 +0530

    Time: 7 ms (87.09%) | Memory: 12 MB (5.43%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -13,50 +13,85 @@
 //     }
 //     return -1;
 // }
-struct hashmap {
-  int val;
-  int index;
-};
-int * twoSum(int * nums, int numsSize, int target, int * returnSize) {
-  int * result = (int * ) malloc(2 * sizeof(int * ));
-  struct hashmap * hp = (struct hashmap * ) malloc((numsSize + 2) * sizeof(struct hashmap * ));
-  int diff = 0;
-  int num;
-  for (int i = 0; i < numsSize; i++) { //int sum=0;
-    int j = 0;
-    num = nums[i];
-    diff = target - nums[i];
-    //if (num != diff) 
-    {
-      hp[i].val = nums[i];
-      hp[i].index = i;
+typedef struct Node {
+    int key;
+    int value;
+    struct Node* next;
+} Node;
+#define HASH_SIZE 20011 // A prime number helps distribute keys evenly
+
+// Simple absolute modulo hash function
+int hash(int key) {
+    int h = key % HASH_SIZE;
+    return h < 0 ? h + HASH_SIZE : h;
+}
+
+// Insert a key-value pair into the hash table
+void insert(Node** hashTable, int key, int value) {
+    int index = hash(key);
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = hashTable[index]; // Insert at the head of the chain
+    hashTable[index] = newNode;
+}
+
+// Search for a key and return its index, or -1 if not found
+int search(Node** hashTable, int key) {
+    int index = hash(key);
+    Node* current = hashTable[index];
+    while (current != NULL) {
+        if (current->key == key) {
+            return current->value;
+        }
+        current = current->next;
     }
-    while (i > 0 && j < i) {
-      if (diff == hp[j].val) {
-        result[0] = hp[j].index;
-        result[1] = i;
-        * returnSize = 2;
-        return result;
-      }
-      j++;
+    return -1;
+}
+
+// Free memory allocated for the hash table
+void freeHashTable(Node** hashTable) {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        Node* current = hashTable[i];
+        while (current != NULL) {
+            Node* temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    // Allocate the hash table buckets initialized to NULL
+    Node** hashTable = (Node**)calloc(HASH_SIZE, sizeof(Node*));
+    
+    // Allocate the result array
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        
+        // Check if the complement is already in the hash table
+        int complementIndex = search(hashTable, complement);
+        if (complementIndex != -1) {
+            result[0] = complementIndex;
+            result[1] = i;
+            *returnSize = 2;
+            
+            freeHashTable(hashTable);
+            free(hashTable);
+            return result;
+        }
+        
+        // If not found, insert the current number and its index
+        insert(hashTable, nums[i], i);
     }
 
-    //   j=find(i,diff,nums,numsSize);
-    //   if(j!=-1)
-    //   {
-    // //  for(int j=i+1;j<numsSize;j++)
-    // //  {
-    // //  sum=nums[i]+nums[j];
-    // //  if(sum==target)
-    // //   {
-    //     result[0]=i;
-    //     result[1]=j;
-    //     *returnSize=2;
-    //     return result;
-    //   }
-    // //   }
-    // //  }
-  }
-  return NULL;
-
+    // Clean up if no solution is found (though LeetCode guarantees one)
+    freeHashTable(hashTable);
+    free(hashTable);
+    return NULL;
 }
