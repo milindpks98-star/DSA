695105b734e4de0482f534cd8e95accf422bdd2a
milindpks98-star <milind.pks98@gmail.com>
2026-05-31 16:49:29 +0530

    Time: 5 ms (95.88%) | Memory: 18.7 MB (14.23%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.c
+++ 217-contains-duplicate/contains-duplicate.c
@@ -1,33 +1,25 @@
 bool containsDuplicate(int* nums, int numsSize)
 {
-    int size = numsSize * 2 + 1;
+    int size=numsSize*2+1;
+    int *table=(int *)calloc(size,sizeof(int));
+    bool *used=(bool *)calloc(size,sizeof(bool));
 
-    int *table = (int *)calloc(size, sizeof(int));
-    bool *used = (bool *)calloc(size, sizeof(bool));
-
-    for (int i = 0; i < numsSize; i++)
+    for(int i=0;i<numsSize;i++)
     {
-        int key = nums[i];
-        int idx = ((key % size) + size) % size;
-
-        while (used[idx])
+        int idx=((nums[i]%size)+size)%size;
+        while(used[idx])
         {
-            if (table[idx] == key)
+            if(table[idx]==nums[i])
             {
                 free(table);
                 free(used);
                 return true;
             }
-
-            idx = (idx + 1) % size;
+            idx=(idx+1)%size;
         }
 
-        table[idx] = key;
-        used[idx] = true;
+        table[idx]=nums[i];
+        used[idx]=true;
     }
-
-    free(table);
-    free(used);
-
     return false;
 }
