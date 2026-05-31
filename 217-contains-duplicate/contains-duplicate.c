0f4949456bbb317d0846f9536171a7f64dfcc30a
milindpks98-star <milind.pks98@gmail.com>
2026-05-31 16:59:03 +0530

    Time: 5 ms (95.88%) | Memory: 18.7 MB (14.11%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.c
+++ 217-contains-duplicate/contains-duplicate.c
@@ -1,7 +1,13 @@
+//calloc is used instead of malloc because calloc Initializes every byte to 0.
+//where as malloc contains garbage value initially
+//Use malloc() when:You are going to initialize every element yourself.
+//Use calloc() when:You need all values to start at zero.
+//                   You're creating counters, frequency tables, hash tables, flags, etc.
+
 bool containsDuplicate(int* nums, int numsSize)
 {
     int size=numsSize*2+1;
-    int *table=(int *)calloc(size,sizeof(int));
+    int *table=(int *)calloc(size,sizeof(int)); 
     bool *used=(bool *)calloc(size,sizeof(bool));
 
     for(int i=0;i<numsSize;i++)
