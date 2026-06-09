b6fb46563961042294105a12dafcdc5e65c327a5
milindpks98-star <milind.pks98@gmail.com>
2026-06-09 17:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (31.76%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.c
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.c
@@ -1,8 +1,16 @@
+//////milind////
+//-DBL_MAX: Lowest finite negative value (approx. -1.79 × 10³⁰⁸).
+//-DBL_MIN: Closest negative value to zero (normalized).
+//0.0: Cente
+//DBL_MIN: Closest positive value to zero (normalized).
+//DBL_MAX: Highest finite positive value (approx. 1.79 × 10³⁰⁸)
+
+
 
 double findMaxAverage(int* nums, int numsSize, int k) {
     int left=0,right=0;
     double sum=0;
-    double maxavg=-DBL_MAX;
+    double maxavg=-DBL_MAX;  //
     int len=1;
     for(right=0;right<numsSize;right++)
     {
