c92209ca6ba4090aa0a6566abc70b3a408ac8302
milindpks98-star <milind.pks98@gmail.com>
2026-06-10 00:38:08 +0530

    Time: 6 ms (30.42%) | Memory: 10.2 MB (8.54%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.c
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.c
@@ -1,34 +1,32 @@
 int maxVowels(char* s, int k) {
-    int left=0;int right=0;
-    int len=strlen(s);
-            int max=0;
-           for(int i=0;i<k;i++) 
-           {
-                if(s[i]=='a' || s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-                {
-                 max++;
-           
-                }
-           } 
-            int maxvo=max;
-            for(int i=k;s[i]!='\0';i++)
-            {
-              if(s[i]=='a' || s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-                {
-                 max++;
-           
-                }
+   int maxvo=0;
+   int max=0;
+   int len=strlen(s);
+   for(int i=0;i<k;i++)
+   {
+    if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+    {
+        max++;
+    }
+   }
+   maxvo=max;
+   for(int i=k;s[i]!='\0';i++)
+   {
+    if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+    {
+        max++;
+    }
+    if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u')
+    {
+        max--;
+    }
+    if(max>maxvo)
+    {
+        maxvo=max;
+    }
+   }
 
-                if(s[i-k]=='a' || s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u')
-                {
-                    max--;
-                }
-                if(max>maxvo)
-                maxvo=max;
-            }
-            
-            return (maxvo==-1?0:maxvo);
-            
+    return maxvo;
 } 
       
    
