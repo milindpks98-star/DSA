263e12a1e2d8273afea72b05a1e960cec06f2375
milindpks98-star <milind.pks98@gmail.com>
2026-06-10 00:38:30 +0530

    Time: 8 ms (17.08%) | Memory: 10.1 MB (47.08%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.c
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.c
@@ -1,7 +1,7 @@
 int maxVowels(char* s, int k) {
    int maxvo=0;
    int max=0;
-   int len=strlen(s);
+
    for(int i=0;i<k;i++)
    {
     if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
