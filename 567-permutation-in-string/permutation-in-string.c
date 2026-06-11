2ce25a3137265ad324b8290f3e67ae10470a91b9
milindpks98-star <milind.pks98@gmail.com>
2026-06-11 19:04:48 +0530

    Time: 4 ms (14.34%) | Memory: 9.6 MB (5.18%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.c
+++ 567-permutation-in-string/permutation-in-string.c
@@ -1,32 +1,33 @@
 
 
 bool checkInclusion(char* s1, char* s2) {
-    int l1 = strlen(s1);
-    int l2 = strlen(s2);
-    
-    if (l1 > l2) return false;
+ int h1[256]={0};
+ int h2[256]={0};
 
-    // Expand arrays to 256 to cover every possible byte value safely
-    int h1[256] = {0};
-    int h2[256] = {0};
-    
-    for (int i = 0; i < l1; i++) {
-       // Using (unsigned char) guarantees a safe index between 0 and 255
-       h1[(unsigned char)s1[i]]++;
-       h2[(unsigned char)s2[i]]++;
-    }
-    
-    if (memcmp(h1, h2, sizeof(h1)) == 0) return true;
+ int left=0;
+int l1=strlen(s1);
+int l2=strlen(s2);
+if(l1>l2)return false;
 
-    for (int i = l1; i < l2; i++) {
-        // Add character entering from the right edge
-        h2[(unsigned char)s2[i]]++;
+for(int i=0;i<l1;i++)
+{
+    h1[(unsigned char)s1[i]]++;
+}
 
-        // Remove character exiting from the left edge
-        h2[(unsigned char)s2[i - l1]]--;
+for(int right=0;right<l2;right++)
+{
+    h2[(unsigned char)s2[right]]++;
 
-        if (memcmp(h1, h2, sizeof(h1)) == 0) return true;
+    while(right-left+1>l1)
+    {
+        h2[(unsigned char)s2[left]]--;
+        left++;
     }
 
-    return false;
+    if(memcmp(h1,h2,sizeof(h1))==0)
+    return true;
+
+
+}
+return false;
 }
