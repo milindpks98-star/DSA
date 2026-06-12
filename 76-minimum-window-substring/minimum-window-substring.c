460b9ece0e26948e53048fd0edc41b24559cd27f
milindpks98-star <milind.pks98@gmail.com>
2026-06-13 01:44:33 +0530

    Time: 7 ms (7.07%) | Memory: 10.8 MB (5.17%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.c
+++ 76-minimum-window-substring/minimum-window-substring.c
@@ -1,60 +1,60 @@
 char* minWindow(char* s, char* t) {
-    int len1=strlen(s);
-    int len2=strlen(t);
-    int left=0;
-    int right;
-    int h1[256]={0};
-    int h2[256]={0};
-    int str_idx=0;
+  int len1=strlen(s);
+  int len2=strlen(t);
+  int left=0;
+  int minsize=INT_MAX;
+  int h1[256]={0};
+  int h2[256]={0};
+  int required_match=0;
+  int formed_match=0; 
+  int str_idx=0;
+
+  for(int i=0;i<len2;i++)
+  {
+    if(h2[(unsigned char)t[i]]==0)
+    {
+        required_match++;
+    }
+    h2[(unsigned char)t[i]]++;
+  }
+
+  for(int right=0;right<len1;right++)
+  {
+    h1[(unsigned char)s[right]]++;
+    if(h2[(unsigned char)s[right]]>0 && h1[(unsigned char)s[right]]==h2[(unsigned char)s[right]])
+    {
+        formed_match++;
+    }
     
 
-    int minsize=INT_MAX;
-    if(len2>len1)
+    while(formed_match==required_match)
+    {
+        int size=right-left+1;
+        if(size<minsize)
+        {
+            minsize=size;
+            str_idx=left;
+        }
+        h1[(unsigned char)s[left]]--;
+        
+        if(h2[(unsigned char)s[left]]>0 && h1[(unsigned char)s[left]]<h2[(unsigned char)s[left]])
+        {
+            formed_match--;
+            
+        }
+        left++;
+    }
+  }
+  if(minsize==INT_MAX)
+  {
     return "";
+  }
 
-    int required_match=0;
-    for(right=0;right<len2;right++)
-    {
-        if(h2[(unsigned char) t[right]]==0)
-        {
-            required_match++;
-        }
-        h2[(unsigned char)t[right]]++;
-    }
+char *result=malloc((minsize+1)*sizeof(char));
+strncpy(result,s+str_idx,minsize);
+result[minsize]='\0';
 
-    int formed_match=0;
-    for(int i=0;i<len1;i++)
-    {
-        h1[(unsigned char)s[i]]++;
-        if(h2[(unsigned char)s[i]]>0 && h1[(unsigned char)s[i]]==h2[(unsigned char)s[i]])
-        {
-            formed_match++;
-        }
+return result;
 
-        while(required_match==formed_match)
-        {
-            int size=i-left+1;
-             if(size<minsize)
-               {
-                minsize=size;
-                str_idx=left;
-               }
-               h1[(unsigned char)s[left]]--;
-            if(h2[(unsigned char)s[left]]>0 && h1[(unsigned char)s[left]]<h2[(unsigned char)s[left]])
-            {
-                formed_match--;
-            }
-            left++;
-        }
-      
-     
-    }
-    if(minsize==INT_MAX)
-     return "";
-
-    char *sub=malloc((minsize+1)*sizeof(char));
-    strncpy(sub,s+str_idx,minsize);
-    sub[minsize]='\0';
-return sub;
 
 }
