7b87810781fa17eb2d10a3d8285244bf2a160193
milindpks98-star <milind.pks98@gmail.com>
2026-06-24 17:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (78.47%) - LeetSync

---
+++ 100-same-tree/same-tree.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p==NULL && q==NULL)
+    return true;
+    else if(p==NULL || q==NULL || p->val!=q->val)
+    return false;
+
+    else
+    {
+        return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+        
+    }
+    return false;
+
+}
