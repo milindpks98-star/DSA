06e332567f973749fa81f68e69f4d57a53d6cdc4
milindpks98-star <milind.pks98@gmail.com>
2026-07-02 02:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (66.67%) - LeetSync

---
+++ 733-flood-fill/flood-fill.c
@@ -0,0 +1,32 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+ void dfs(int** image, int rows, int coloumns, int r, int c,int old_color, int new_color)
+ {
+    
+    if(r<0||r>=rows||c<0||c>=coloumns)
+    return;
+    if(image[r][c]!=old_color)
+    return;
+    image[r][c]=new_color;
+
+    dfs(image,rows,coloumns,r-1,c,old_color,new_color);//up
+    dfs(image,rows,coloumns,r+1,c,old_color,new_color);//down
+    dfs(image,rows,coloumns,r,c-1,old_color,new_color);//left
+    dfs(image,rows,coloumns,r,c+1,old_color,new_color);//right
+
+ }
+int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int* returnSize, int** returnColumnSizes) {
+    *returnSize=imageSize;
+    *returnColumnSizes=imageColSize;
+
+    int old_color=image[sr][sc];
+    if(old_color==color)
+    return image;
+
+    dfs(image,imageSize,imageColSize[0],sr,sc,old_color,color);
+
+    return image;
+}
