d7a9768199f49b85741bb2bc2ba04415e2e44bbb
milindpks98-star <milind.pks98@gmail.com>
2026-07-04 19:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (48.35%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.c
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.c
@@ -2,35 +2,45 @@
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
- //2nd submission..no change only practice
+ */ 
+ //change for pathsize and keeping for loop inside loop
  int **result;
  int *path;
- int  pathsize;
- void dfs(int **graph,int graphSize,int *graphColSize,int node,int *returnSize,int **returnColumnSizes)
+ int pathsize;
+ void dfs(int **graph,int graphSize,int* graphColSize,int node,int* returnSize,int **returnColumnSizes)
  {
     path[pathsize++]=node;
     if(node==graphSize-1)
     {
-        result[*returnSize]=malloc(pathsize*sizeof(int *));
-        memcpy(result[*returnSize],path,pathsize*sizeof(int));
+        result[*returnSize]=malloc(pathsize*sizeof(int));
+        memcpy(result[*returnSize], path, pathsize * sizeof(int));
         (*returnColumnSizes)[*returnSize]=pathsize;
         (*returnSize)++;
-        pathsize--;
-        return;
+
+       // pathsize--;
+       //return;
     }
-    for(int i=0;i<graphColSize[node];i++)
+    
+    else
     {
-        dfs(graph,graphSize,graphColSize,graph[node][i],returnSize,returnColumnSizes);
+        
+        for(int i=0;i<graphColSize[node];i++)
+           
+           { 
+            dfs(graph,graphSize,graphColSize,graph[node][i],returnSize,returnColumnSizes);
+           }
+       
     }
     pathsize--;
+
  }
 int** allPathsSourceTarget(int** graph, int graphSize, int* graphColSize, int* returnSize, int** returnColumnSizes) {
-    result=malloc(10000*sizeof(int*));
+    result=malloc(10000*sizeof(int *));
     *returnColumnSizes=malloc(10000*sizeof(int));
-    path=malloc(graphSize*sizeof(int));
+    path =malloc(graphSize*sizeof(int));
     pathsize=0;
     *returnSize=0;
+  
     dfs(graph,graphSize,graphColSize,0,returnSize,returnColumnSizes);
     return result;
 }
