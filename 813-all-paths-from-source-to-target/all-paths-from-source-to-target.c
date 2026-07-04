4efa837fbf29331ebed9f11600c1fdb18103de35
milindpks98-star <milind.pks98@gmail.com>
2026-07-04 18:58:24 +0530

    Time: 4 ms (41.76%) | Memory: 21.8 MB (35.16%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.c
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.c
@@ -3,41 +3,34 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
+ //2nd submission..no change only practice
  int **result;
  int *path;
- int pathsize;
- void dfs(int **graph,int graphSize,int* graphColSize,int node,int* returnSize,int **returnColumnSizes)
+ int  pathsize;
+ void dfs(int **graph,int graphSize,int *graphColSize,int node,int *returnSize,int **returnColumnSizes)
  {
     path[pathsize++]=node;
     if(node==graphSize-1)
     {
         result[*returnSize]=malloc(pathsize*sizeof(int *));
-        memcpy(result[*returnSize], path, pathsize * sizeof(int));
+        memcpy(result[*returnSize],path,pathsize*sizeof(int));
         (*returnColumnSizes)[*returnSize]=pathsize;
         (*returnSize)++;
-
         pathsize--;
         return;
     }
-    
     for(int i=0;i<graphColSize[node];i++)
     {
-        
-        
-           
-            dfs(graph,graphSize,graphColSize,graph[node][i],returnSize,returnColumnSizes);
-       
+        dfs(graph,graphSize,graphColSize,graph[node][i],returnSize,returnColumnSizes);
     }
     pathsize--;
-
  }
 int** allPathsSourceTarget(int** graph, int graphSize, int* graphColSize, int* returnSize, int** returnColumnSizes) {
-    result=malloc(10000*sizeof(int *));
+    result=malloc(10000*sizeof(int*));
     *returnColumnSizes=malloc(10000*sizeof(int));
-    path =malloc(graphSize*sizeof(int));
+    path=malloc(graphSize*sizeof(int));
     pathsize=0;
     *returnSize=0;
-  
     dfs(graph,graphSize,graphColSize,0,returnSize,returnColumnSizes);
     return result;
 }
