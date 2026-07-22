3a6a770e31742db211c5a9393ad93e28a5af44a6
milindpks98-star <milind.pks98@gmail.com>
2026-07-22 18:40:21 +0530

    Time: 1 ms (26.90%) | Memory: 9.5 MB (5.22%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int solve(int i,int j,int m, int n,vector<vector<int>>&dp)
     {
-        if(i>=m || j>=n)return 0;//beyond boudary
+        if(i>=m || j>=n)return 0;//beyond boundary
         if(i==m-1 || j==n-1)return 1;//destinaton
         if(dp[i][j]!=-1)return dp[i][j];
         dp[i][j]=solve(i+1,j,m,n,dp) + solve(i,j+1,m,n,dp);
