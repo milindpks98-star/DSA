b8da0f16f920f8f2ab66ada559ca123f751ecb1b
milindpks98-star <milind.pks98@gmail.com>
2026-07-25 18:14:14 +0530

    Time: 23 ms (70.35%) | Memory: 18.9 MB (60.23%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int>&coins,int amount,vector<int>&dp)
+    {
+        
+        if(amount==0)
+        return 0;
+        if(amount<0)
+        return 1e9;
+        int ans=1e9;
+        if(dp[amount]!=-1)
+        return dp[amount];
+            for(int coin:coins)
+            {  
+                     ans=min(ans,1+solve(coins,amount-coin,dp));
+            }
+        
+       return dp[amount]=ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,-1);
+        int ans= solve(coins,amount,dp);
+        if(ans>=1e9)
+        return -1;
+
+        return ans;
+    }
+};
