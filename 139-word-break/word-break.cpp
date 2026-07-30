30eae99255b08c3fa49b7bbbc205c5b0e817480a
milindpks98-star <milind.pks98@gmail.com>
2026-07-30 19:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (73.27%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(int index,string &s,vector<string>&wordDict,vector<int> &dp)
+    {
+        if(index==s.size())return true;
+        if(dp[index]!=-1)return dp[index];
+        for(string word:wordDict)
+        {
+            int len=word.size();
+            if(index+len<=s.size() && s.substr(index,len)==word)
+            {
+                    if(solve(index+len,s,wordDict,dp))
+                    return dp[index]=true;
+            }
+        }
+       return dp[index]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> dp(s.size(),-1);
+        return solve(0,s,wordDict,dp);
+    }
+};
