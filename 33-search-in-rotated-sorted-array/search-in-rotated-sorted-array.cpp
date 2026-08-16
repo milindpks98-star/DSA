baba1b9d01bc56f533ca5247df054a7fd821c425
milindpks98-star <milind.pks98@gmail.com>
2026-08-16 12:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (69.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -9,21 +9,26 @@ public:
             if(target==nums[mid])return mid;
             if(nums[s]<=nums[mid])
             {
+
                 if(target>=nums[s] && target<nums[mid])
                 {
-                    l=mid-1;
+                     l=mid-1;
                 }
-                else s=mid+1;
+                else
+                {
+                    s=mid+1;
+                }
             }
-            else 
+            else
             {
                 if(target>nums[mid] && target<=nums[l])
                 {
                     s=mid+1;
                 }
-                else l=mid-1;
-
-            
+                else
+                {
+                    l=mid-1;
+                }
             }
         }
         return -1;
